e92e89cb364c79042b8ae5168332d59dee0ed535
amanddali <league@WTS20.attlocal.net>
2017-04-25 18:07:39 -0700

    sdfgdfsg

--- level 1/src/Houses.java
+++ level 1/src/Houses.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 
+import javax.swing.JOptionPane;
+
 import org.teachingextensions.logo.Tortoise;
 
 public class Houses {
@@ -9,23 +11,72 @@ public class Houses {
 		Tortoise.setY(400);
 		Tortoise.show();
 		Tortoise.penDown();
+		String textsize = JOptionPane.showInputDialog(
+				"what size do you want your houses to be.Large medium or small(make the first letter capital)");
+		String textcolor = JOptionPane
+				.showInputDialog("What color do you want your houses to be? Red, Pink or Blue (first letter CAPITAL)");
+
 		for (int j = 0; j < 10; j++) {
-			drawHouse(300, 40);
+			drawHouse(textsize, 40, textcolor);
 		}
+
 	}
 
-	public static void drawHouse(int size, int length) {
+	public static void drawHouse(String textsize, int length, String textcolor) {
+		int size = 0;
+		if (textsize.equals("Small")) {
+			size = 60;
+		}
+		if (textsize.equals("Medium")) {
+			size = 120;
+		}
+		if (textsize.equals("Large")) {
+			size = 250;
+		}
 
-		Tortoise.setPenColor(Color.BLACK);
+		Color color = null;
+		if (textcolor.equals("Red")) {
+			color = Color.RED;
+
+		}
+		if (textcolor.equals("Pink")) {
+			color = Color.PINK;
+
+		}
+		if (textcolor.equals("Blue")) {
+			color = Color.BLUE;
+
+		}
+		Tortoise.setPenColor(color);
 		Tortoise.move(size);
-		Tortoise.turn(90);
-		Tortoise.move(length);
-		Tortoise.turn(90);
+		// Tortoise.turn(90);
+		// Tortoise.move(length);
+		// Tortoise.turn(90);
+		if (textsize.equals("Large")) {
+			drawFlatRoof(length);
+		} else {
+			drawPointyRoof();
+		}
 		Tortoise.move(size);
 		Tortoise.setPenColor(Color.GREEN);
 		Tortoise.turn(-90);
 		Tortoise.move(20);
 		Tortoise.turn(-90);
+
+	}
+
+	public static void drawPointyRoof() {
+		Tortoise.turn(30);
+		Tortoise.move(30);
+		Tortoise.turn(120);
+		Tortoise.move(30);
+		Tortoise.turn(30);
+	}
+
+	public static void drawFlatRoof(int length) {
+		Tortoise.turn(90);
+		Tortoise.move(length);
+		Tortoise.turn(90);
 	}
 }
 
