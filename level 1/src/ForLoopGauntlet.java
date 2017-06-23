c0441e1db811be876779bd22538b3024562ea52e
RhymeLyet <RhymeInParentheses@gmail.com>
2017-06-23 11:04:48 -0700

    jgkjgkjgkjgkj

---
+++ level 1/src/ForLoopGauntlet.java
@@ -0,0 +1,47 @@
+
+public class ForLoopGauntlet {
+	public static void main(String[] args) {
+		// for (int i = 0; i < 101; i++) {
+		// System.out.println(i);
+		//
+		// }
+		// for (int j = 100; j > 0; j--) {
+		// System.out.println(j);
+		//
+		// }
+		// for (int i = 0; i < 101; i++) {
+		// if (i % 2 == 0) {
+		// System.out.println(i);
+		// }
+		// }
+		// for (int i = 0; i < 100; i++) {
+		// if (i % 2 == 1) {
+		// System.out.println(i);
+		//
+		// }
+		//
+		// }
+		// for (int j = 0; j < 501; j++) {
+		// if (j % 2 == 0) {
+		// System.out.println(j + " even");
+		// }
+		// if (j % 2 == 1) {
+		// System.out.println(j + " odd");
+		// }
+		//
+		// }
+		// for (int i = 0; i < 778; i++) {
+		// if (i % 7 == 0) {
+		// System.out.println(i);
+		// }
+		//
+		// }
+		// for (int i = 2006; i <= 2017; i++) {
+		//
+		// System.out.println("In " + i + " I was " + (i - 2006) + " years
+		// old");
+		//
+		// }
+
+	}
+}
